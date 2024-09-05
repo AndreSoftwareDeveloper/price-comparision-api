582f8f7bc82b976f37dd80882ae72ba387af0d64
YoungNavigator <andriej2301@gmail.com>
2024-09-05 17:52:26 +0200

    feat: Offer model

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from databases import Database
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
-from sqlalchemy import MetaData, select
+
+from sqlalchemy import MetaData, select, join
 from sqlalchemy.ext.declarative import declarative_base
-from starlette.middleware.cors import CORSMiddleware
-from models import Product
+
+from models import Product, Offer
 
 DATABASE_URL = "postgresql+asyncpg://postgres:admin@localhost/PriceComparision"
 
@@ -35,10 +37,22 @@ async def shutdown():
 
 @app.get("/")
 async def search_offers(name_or_category: str):
-    query = select(Product).where(
-        (Product.name.ilike(f"%{name_or_category}%")) |
-        (Product.category.ilike(f"%{name_or_category}%"))
+    query = (
+        select(
+            Product.category,
+            Product.name,
+            Offer.shop,
+            Offer.price
+        )
+        .select_from(
+            join(Product, Offer, Product.id == Offer.product_id)
+        )
+        .where(
+            (Product.name.ilike(f"%{name_or_category}%")) |
+            (Product.category.ilike(f"%{name_or_category}%"))
+        )
     )
+
     results = await database.fetch_all(query)
     products = [dict(result) for result in results]
     return JSONResponse(status_code=200, content={"products": products})
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Numeric
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -12,3 +12,12 @@ class Product(Base):
     id = Column(Integer, primary_key=True, index=True)
     category = Column(String, index=True)
     name = Column(String, index=True)
+
+
+class Offer(Base):
+    __tablename__ = "offers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer, primary_key=False, index=True)
+    shop = Column(String, index=True)
+    price = Column(Numeric(precision=10, scale=2))
